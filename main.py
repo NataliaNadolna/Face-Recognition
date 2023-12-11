435cfacbc86384194b4818b82fa259517b136d9e
Natalia Nadolna <253230@student.pwr.edu.pl>
2023-12-11 12:21:45 +0100

    Added models

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import numpy as np
 import cv2
- 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-#face_cascade = cv2.CascadeClassifier('haarcascade_eye.xml.xml')
-#face_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 class Recognition:
+
+    def __init__(self):
+        self.model = cv2.CascadeClassifier('models/haarcascade_frontalface_alt2.xml')
+        #self.model = cv2.CascadeClassifier('models/haarcascade_eye.xml.xml')
+        #self.model = cv2.CascadeClassifier('models/haarcascade_smile.xml')
+
     def onCamera(self):
         cap = cv2.VideoCapture(0)
  
         while(True):
             ret, img = cap.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+            faces = self.model.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
  
             for (x,y,w,h) in faces:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(190,0,0),2)
@@ -26,7 +28,7 @@ class Recognition:
 
     def onImage(self, imgPath: str): # nie działa
         img = cv2.imread(imgPath)
-        faces = face_cascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
+        faces = self.model.detectMultiScale(img, scaleFactor=1.05, minNeighbors=5)
  
         for (x,y,w,h) in faces:
             cv2.rectangle(img,(x,y),(x+w,y+h),(190,0,0),2)
@@ -46,7 +48,7 @@ class Recognition:
         while sucess:
             ret, img = cap.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+            faces = self.model.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
  
             for (x,y,w,h) in faces:
                 cv2.rectangle(img,(x,y),(x+w,y+h),(190,0,0),2)
@@ -58,11 +60,8 @@ class Recognition:
         cap.release()
         cv2.destroyAllWindows()
 
-
-
-    
 recognise = Recognition()
-#recognise.onCamera()
-recognise.onImage("twarze\Natalia\363278905_331444039240104_4662878192679284882_n.jpg")
+recognise.onCamera()
+#recognise.onImage("twarze\Natalia\363278905_331444039240104_4662878192679284882_n.jpg")
 #recognise.onVideo("film.mp4")
  
